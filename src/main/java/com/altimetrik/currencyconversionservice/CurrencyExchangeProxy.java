e1571b5305866d69acfe1ee282f88be8ac59ef50
madhankumar <madhan2007.psgtech@gmail.com>
2020-01-06 16:50:51 +0530

    ribbon load balancer example

--- src/main/java/com/altimetrik/currencyconversionservice/CurrencyExchangeProxy.java
+++ src/main/java/com/altimetrik/currencyconversionservice/CurrencyExchangeProxy.java
@@ -1,12 +1,14 @@
 package com.altimetrik.currencyconversionservice;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.altimetrik.currencyconversionservice.model.CurrencyConversionBean;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8001")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean getExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
