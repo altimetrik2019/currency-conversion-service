0dd8b5071666c1af50b8e1bf4ce76ffc31f9973d
madhankumar <madhan2007.psgtech@gmail.com>
2020-03-04 15:57:01 +0530

    final commits

--- src/main/java/com/altimetrik/currencyconversionservice/CurrencyExchangeProxy.java
+++ src/main/java/com/altimetrik/currencyconversionservice/CurrencyExchangeProxy.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.altimetrik.currencyconversionservice.model.CurrencyConversionBean;
 
-@FeignClient(name = "currency-exchange-service")
+//@FeignClient(name = "currency-exchange-service", url="localhost:8000")
+//@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    // @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean getExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 }
--- src/main/java/com/altimetrik/currencyconversionservice/web/CurrencyConversionController.java
+++ src/main/java/com/altimetrik/currencyconversionservice/web/CurrencyConversionController.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,8 @@ import com.altimetrik.currencyconversionservice.model.CurrencyConversionBean;
 @RestController
 public class CurrencyConversionController {
     
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private CurrencyExchangeProxy currencyExchangeProxy;
 
@@ -39,6 +43,7 @@ public class CurrencyConversionController {
     public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
             @PathVariable BigDecimal quantity) {
         CurrencyConversionBean response = currencyExchangeProxy.getExchangeValue(from, to);
+        logger.info("{}", response.getId());
         return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
                 quantity.multiply(response.getConversionMultiple()), response.getPort());
 
